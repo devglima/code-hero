e540ed1dc91b4b9fb88897f67d03f9a53901451e
Guilherme Lima <guilherme.santos@simoninisoftware.com.br>
2024-05-15 21:11:36 -0300

    Remove unnecessary code

--- lib/utils/button_style.dart
+++ lib/utils/button_style.dart
@@ -1,34 +1,10 @@
 import 'package:flutter/material.dart';
 
-buttonStyle(BuildContext context) {
-  return TextButton.styleFrom(
-    textStyle: TextStyle(
-      color: Theme.of(context).hintColor,
-    ),
-  );
-}
-
-cartButtonStyle(BuildContext context) {
-  return TextButton.styleFrom(
-    padding: const EdgeInsets.symmetric(vertical: 14),
-    backgroundColor: Theme.of(context).colorScheme.secondary,
-    shape: const StadiumBorder(),
-  );
-}
-
 buttonStyleStadium(BuildContext context) {
   return TextButton.styleFrom(
     padding: const EdgeInsets.symmetric(vertical: 14),
     backgroundColor: Colors.red,
     shape: const StadiumBorder(),
     minimumSize: const Size(230.0, 0),
-  );
-}
-
-buttonStyleStadiumWithoutOpacity(BuildContext context) {
-  return TextButton.styleFrom(
-    padding: const EdgeInsets.symmetric(vertical: 14),
-    backgroundColor: Theme.of(context).colorScheme.secondary,
-    shape: const StadiumBorder(),
   );
 }
